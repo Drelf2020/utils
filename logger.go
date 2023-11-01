2768952a208a9ca060505729c3f6a2109c0f5965
Drelf2018 <drelf2018@outlook.com>
2023-11-01 12:32:32 +0800

    feat: CloseLogFile

--- logger.go
+++ logger.go
@@ -8,32 +8,39 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// 基础输出格式
-var log = &logrus.Logger{
-	Out: os.Stderr,
-	Formatter: &nested.Formatter{
-		HideKeys:        true,
-		NoColors:        true,
-		TimestampFormat: "15:04:05",
-	},
-	Level: logrus.DebugLevel,
-}
+var (
+	// 日志文件
+	logfile *os.File
+	// 基础输出格式
+	log = &logrus.Logger{
+		Out: os.Stderr,
+		Formatter: &nested.Formatter{
+			HideKeys:        true,
+			NoColors:        true,
+			TimestampFormat: "15:04:05",
+		},
+		Level: logrus.DebugLevel,
+	}
+)
 
 // 获取 log
-func GetLog() *logrus.Logger {
+func Logger() *logrus.Logger {
 	return log
+}
+
+// 关闭日志文件
+func CloseLogFile() error {
+	if logfile != nil {
+		return logfile.Close()
+	}
+	return nil
 }
 
 // 导出文件
 func SetOutputFile(path string) *logrus.Logger {
-	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
-	PanicErr(err)
-	writers := []io.Writer{
-		file,
-		os.Stdout,
-	}
-	fileAndStdoutWriter := io.MultiWriter(writers...)
-	log.SetOutput(fileAndStdoutWriter)
+	CloseLogFile()
+	logfile = Panic(os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm))
+	log.SetOutput(io.MultiWriter(logfile, os.Stdout))
 	return log
 }
 
