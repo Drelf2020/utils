26adafc8cf18965f09bc84e71885538e31a5f11c
Drelf2018 <drelf2018@outlook.com>
2023-11-01 12:32:54 +0800

    feat: Panic+Copy

--- error.go
+++ error.go
@@ -6,21 +6,21 @@ import (
 	"runtime"
 )
 
-// 检查错误 存在错误返回 true
-func CheckErr(err error) bool {
-	return err != nil
-}
-
 // 存在错误则 panic
 func PanicErr(err error) {
-	if CheckErr(err) {
+	if err != nil {
 		panic(err)
 	}
 }
 
+func Panic[T any](data T, err error) T {
+	PanicErr(err)
+	return data
+}
+
 // 存在错误则打印错误并返回 true
 func LogErr(err error) bool {
-	if CheckErr(err) {
+	if err != nil {
 		_, file, line, ok := runtime.Caller(1)
 		if ok {
 			log.Errorf("%v occured in %v:%v", err, file, line)
@@ -34,7 +34,7 @@ func LogErr(err error) bool {
 
 // The Try function allow you run a code in f function without catching the error.
 //
-// When an error raised, is will be automatic capture
+// When an error raised, it will be automatic capture
 // and send into catch functions as a parameter.
 //
 // If there is no error, nothing will happen.
@@ -43,11 +43,15 @@ func Try(f func(), catch ...func(error)) (e error) {
 		if r := recover(); r != nil {
 			e = fmt.Errorf("%v", r)
 			for _, c := range catch {
-				c(e)
+				if c != nil {
+					c(e)
+				}
 			}
 		}
 	}()
-	f()
+	if f != nil {
+		f()
+	}
 	return nil
 }
 
@@ -57,4 +61,9 @@ func Try(f func(), catch ...func(error)) (e error) {
 // or dst can't be set.
 func CopyAny(dst, src any) error {
 	return Try(func() { reflect.ValueOf(dst).Elem().Set(reflect.ValueOf(src)) })
+}
+
+// The Copy function with type parameter.
+func Copy[T any](dst *T, src T) error {
+	return CopyAny(dst, src)
 }
